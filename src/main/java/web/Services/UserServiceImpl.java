cea768564ff9d134081e30bdc03f8f1e6fc0e4be
Нигматулин Эмиль Робертович <ENigmatulin@ibs.root.ibscorp.ru>
2022-07-14 18:55:59 +0500

    Initial commit

---
+++ src/main/java/web/Services/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package web.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.Role;
+import web.model.User;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class  UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao, RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+        addDefaultUser();
+    }
+
+    @Override
+    public User passwordCoder(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(passwordCoder(user));
+    }
+
+    @Override
+    public void removeUser(long id) {
+        userDao.removeUser(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(passwordCoder(user));
+    }
+
+    @Override
+    public User getUserByLogin(String username) {
+        return userDao.getUserByLogin(username);
+    }
+
+    @Override
+    public void addDefaultUser() {
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(roleService.findById(1L));
+        Set<Role> roleSet2 = new HashSet<>();
+        roleSet2.add(roleService.findById(1L));
+        roleSet2.add(roleService.findById(2L));
+        User user1 = new User("Ara","root","root",roleSet);
+        User user2 = new User("Petuh", "crud","crud",roleSet2);
+        addUser(user1);
+        addUser(user2);
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,122 @@
+package web.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="username")
+    private String username;
+    @Column(name="password")
+    private String password;
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Transient
+    private String confirmPassword;
+
+    public User(String name, String username, String password, Set<Role> roles) {
+        this.name = name;
+        this.password = password;
+        this.roles = roles;
+        this.username = username;
+    }
+
+    public User() {
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
